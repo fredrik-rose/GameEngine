852273be5d0a6a15826dd28db802e5efd5ab743c
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-11-20 21:39:44 +0100

    Add TestFramework library
    A really simple unit test framework. It basically consists of a
    couple of asserts and a test case runner.
    
    It is quite easy to use as the following example shows.
    
    static int test_my_function(void)
    {
        TF_assert(my_function() == 1);
    }
    
    int main(int argc, char *argv[])
    {
        TF_test_case test_cases[] = {
            test_my_function,
        };
    
        return TF_run_suite(test_cases, 1));
    }

---
+++ src/TestFramework/test_framework.c
@@ -0,0 +1,29 @@
+/**
+ * \brief Implementation of a minimalistic unit test framework
+ */
+#include "test_framework.h"
+
+int TF_run_suite(
+    TF_test_case test_cases[],
+    const int number_of_test_cases)
+{
+    int failed = 0;
+
+    for (int i = 0; i < number_of_test_cases; ++i)
+    {
+        if (test_cases[i]())
+        {
+            ++failed;
+        }
+    }
+
+    if (failed)
+    {
+        printf("Ran %d tests, %d FAILED\n", number_of_test_cases, failed);
+        return -1;
+    }
+
+    printf("All %d tests PASSED\n", number_of_test_cases);
+
+    return 0;
+}
---
+++ src/TestFramework/test_framework.h
@@ -0,0 +1,32 @@
+/**
+ * \brief Interface of a minimalistic unit test framework
+ */
+#include <math.h>
+#include <stdio.h>
+
+/** Assert that a certain expression is true */
+#define TF_assert(expression) \
+    do { \
+        if (!(expression)) { \
+            printf("%s:%d %s FAILED\n%s\n", __FILE__, __LINE__, __func__, #expression); \
+            return -1; \
+        } \
+    } while (0)
+
+/** Assert that two floating point values are equal */
+#define TF_assert_double_eq(a, b, granularity) TF_assert(fabs((a) - (b)) < (granularity))
+
+/** Prototype of a test case */
+typedef int (*TF_test_case)(void);
+
+/**
+ * \brief Run a test suite (a collection of test cases)
+ *
+ * \param[int] test_cases The test cases
+ * \param[in] number_of_test_cases The number of test cases
+ *
+ * \return 0 on success a non-zero value otherwise
+ */
+int TF_run_suite(
+    TF_test_case test_cases[],
+    int number_of_test_cases);
