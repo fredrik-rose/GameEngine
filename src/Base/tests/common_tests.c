75db0c143a9a620ec67d6c48c5873ccc92aaa696
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-11-22 21:48:28 +0100

    Add Common tests

---
+++ src/Base/tests/common_tests.c
@@ -0,0 +1,26 @@
+#include "../common.h"
+
+#include "TestFramework/test_framework.h"
+
+int TF_test_case_status;
+
+static void test_LENGTH(void)
+{
+    const int array[] = {1, 2, 3};
+
+    UNUSED(array);
+
+    TF_assert(LENGTH(array) == 3);
+}
+
+int main(int argc, char *argv[])
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    TF_test_case test_cases[] = {
+        test_LENGTH,
+    };
+
+    return TF_run_suite(test_cases, LENGTH(test_cases));
+}
