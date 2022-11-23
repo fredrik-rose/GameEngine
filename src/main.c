af3b02f9a68cfa4a41c693a4acce2616285958eb
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-11-23 22:29:54 +0100

    Run the game from main
    Also clean up all the junk code that was used for
    testing/verification.

--- src/main.c
+++ src/main.c
@@ -1,31 +1,10 @@
 #include "Base/common.h"
-#include "LinearAlgebra/matrix.h"
-#include "LinearAlgebra/vector.h"
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "Game/Game.h"
 
 int main(int argc, char *argv[])
 {
     UNUSED(argc);
     UNUSED(argv);
 
-    const struct VEC_Vector vector = {
-        .length = 4,
-        .data = calloc(4, sizeof(*vector.data))
-    };
-
-    const double vector_element = VEC_get_element(&vector, 0);
-
-    printf("Vector element: %lf\n", vector_element);
-
-    const struct MAT_Matrix matrix = {
-        .rows = 4,
-        .cols = 4,
-        .data = calloc(4 * 4, sizeof(*matrix.data))
-    };
-
-    const double matrix_element = MAT_get_element(&matrix, 0, 0);
-
-    printf("Matrix element: %lf\n", matrix_element);
+    GAME_run();
 }
