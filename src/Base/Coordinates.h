f1e567a8f506dcc471734d8519227b7433d5e02b
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-11-23 17:19:00 +0100

    Add Coordinates to Base
    Contains the definition of 2D and 3D coordinates.

---
+++ src/Base/Coordinates.h
@@ -0,0 +1,21 @@
+/**
+ * \file
+ * \brief Coordinates interface
+ */
+#ifndef BASE_COORDINATES_H
+#define BASE_COORDINATES_H
+
+struct COORD_Coordinate2D
+{
+    double x;
+    double y;
+};
+
+struct COORD_Coordinate3D
+{
+    double x;
+    double y;
+    double z;
+};
+
+#endif /* BASE_COORDINATES_H */
