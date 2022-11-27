2ce274051907f1f1f330284d3c17ea402defa3aa
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-11-27 23:45:58 +0100

    Exclude tests and test framework from coverage report
    These are not of interest, they just add noise to the test
    coverage report.

--- cmake/lcov.cmake
+++ cmake/lcov.cmake
@@ -1,7 +1,9 @@
 add_custom_command(OUTPUT _run_lcov_parser
     POST_BUILD
     COMMAND lcov --no-external --capture --rc lcov_branch_coverage=1 --directory ${CMAKE_SOURCE_DIR}/src
-        --directory ${CMAKE_BINARY_DIR}/src --output-file ${CMAKE_BINARY_DIR}/coverage.info
+        --directory ${CMAKE_BINARY_DIR}/src --output-file ${CMAKE_BINARY_DIR}/unfiltered_coverage.info
+    COMMAND lcov --rc lcov_branch_coverage=1 --remove ${CMAKE_BINARY_DIR}/unfiltered_coverage.info
+        '*/tests/*' '*/TestFramework/*' --output-file ${CMAKE_BINARY_DIR}/coverage.info
     COMMAND genhtml --rc lcov_branch_coverage=1 --ignore-errors source ${CMAKE_BINARY_DIR}/coverage.info
         --output-directory lcov
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
