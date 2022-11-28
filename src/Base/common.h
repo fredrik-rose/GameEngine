36e965d8decf30e527ec686545092b9ed1ad2658
Fredrik Rose <fredrik.rose.developer@gmail.com>
2022-11-28 23:04:42 +0100

    Measure the actual execution time in Game profiler
    Can be used to measure the effect of optimizations.

--- src/Base/common.h
+++ src/Base/common.h
@@ -5,10 +5,36 @@
 #ifndef BASE_COMMON_H
 #define BASE_COMMON_H
 
+#define _GNU_SOURCE
+
+#include <sched.h>
+#include <stdio.h>
+#include <time.h>
+
 /** Use to specify that a function parameter is not used */
 #define UNUSED(param) ((void)(param))
 
 /** Get the number of elements of an array (pointers are not supported) */
 #define LENGTH(array) (sizeof(array) / sizeof(*(array)))
+
+/** Measure the CPU time of a function.
+ * Inputs:
+ *     function: The function to execute
+ * Outputs
+ *     cpu: The CPU on which the function was run (unsigned int)
+ *     node: The CPU node on which the function was run (unsigned int)
+ *     execution_time: The CPU execution time (long long)
+ */
+#define TIME_AND_RUN(function, cpu, node, execution_time, ...) \
+    do { \
+        struct timespec start; \
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start); \
+        function(__VA_ARGS__); \
+        struct timespec end; \
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); \
+        getcpu(&cpu, &node); \
+        execution_time = \
+            ((end.tv_sec - start.tv_sec) * 1000000000LL) + (end.tv_nsec - start.tv_nsec); \
+    } while(0)
 
 #endif /* BASE_COMMON_H */
--- src/Game/profile/game_profiler.c
+++ src/Game/profile/game_profiler.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[])
      * Linux running in a terminal.
      */
     fclose(stdout);
-    GAME_run(FPS, STEPS);
+    GAME_run(FPS, STEPS / 2); /* Warm up, to make sure the CPU enters performance state. */
+    unsigned int cpu = 0U;
+    unsigned int node = 0U;
+    long long execution_time = 0LL;
+    TIME_AND_RUN(GAME_run, cpu, node, execution_time, FPS, STEPS);
     stdout = fopen("/dev/tty","we");
+    printf("Execution time: %lld ns, CPU: %u, Node: %u\n", execution_time, cpu, node);
 }
